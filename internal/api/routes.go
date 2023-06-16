88a07bb297182550253ec14b2ed7d9ceba8b1340
ahmed-023 <ahmed.nadeem@kryptomind.com>
2023-06-16 22:34:04 +0500

    feat: binance history

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -34,5 +34,5 @@ func (r *Server) InitializeRoutes() {
 	// Binance Routes
 	s.HandleFunc("/binance/account", middleware.MiddlewareJSON(r.GetBinanceAccountDetailsData)).Methods("GET")
 	s.HandleFunc("/binance/positions", middleware.MiddlewareJSON(r.GetBinanceAccountPositionsData)).Methods("GET")
-
+	s.HandleFunc("/binance/orderHistory", middleware.MiddlewareJSON(r.GetBinanceOrderHistory)).Methods("GET")
 }
--- response/json.go
+++ response/json.go
@@ -7,6 +7,7 @@ import (
 )
 
 func JSON(w http.ResponseWriter, status int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	err := json.NewEncoder(w).Encode(data)
 	if err != nil {
